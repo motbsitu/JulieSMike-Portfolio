0dc9e2032a9e77de434deee2517c27ef9bc104a2
Julie S Mike <juliesmike@gmail.com>
2017-01-10 22:43:28 -0600

    add contact icons, work on resume

--- script.js
+++ script.js
@@ -11,4 +11,13 @@ $('a[href*="#"]:not([href="#"])').click(function() {
     }
   }
 });
+$(function() {
+  prettyPrint();
+  $('.lang-html').mouseover(function() {
+     $(this).addClass('over');
+  });
+  $('.lang-html').mouseleave(function() {
+    $(this).removeClass('over');
+  });
+});
 });
