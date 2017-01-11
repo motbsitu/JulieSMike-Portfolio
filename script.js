054f14f6f28890b9c56c8cbcbd7dbcb6928115aa
Julie S Mike <juliesmike@gmail.com>
2017-01-11 14:10:40 -0600

    flexboxes for projects and social icons, content, fixed scroll

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
 $(function() {
+
+  // var navbarHeight = $("#navbar").height();
 $('a[href*="#"]:not([href="#"])').click(function() {
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       $('html, body').animate({
-        scrollTop: target.offset().top
+        scrollTop: target.offset().top - 200;
       }, 1000);
       return false;
     }
   }
 });
-$(function() {
-  prettyPrint();
-  $('.lang-html').mouseover(function() {
-     $(this).addClass('over');
-  });
-  $('.lang-html').mouseleave(function() {
-    $(this).removeClass('over');
-  });
-});
+// $(function() {
+//   prettyPrint();
+//   $('.lang-html').mouseover(function() {
+//      $(this).addClass('over');
+//   });
+//   $('.lang-html').mouseleave(function() {
+//     $(this).removeClass('over');
+//   });
+// });
 });
