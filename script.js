90bd552a299af7aa39b29f6b30156c49a0982c3b
Julie S Mike <juliesmike@gmail.com>
2017-01-13 13:12:22 -0600

    Change contact icons

--- script.js
+++ script.js
@@ -13,13 +13,5 @@ $(function() {
     }
   });
 
-// $(function() {
-//   prettyPrint();
-//   $('.lang-html').mouseover(function() {
-//      $(this).addClass('over');
-//   });
-//   $('.lang-html').mouseleave(function() {
-//     $(this).removeClass('over');
-//   });
-// });
+
 });
